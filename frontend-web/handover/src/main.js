525f72d20e34941c412ea521ed3f53f821e788be
Andrew Kim <kskkim@ucdavis.edu>
2024-01-27 12:43:11 -0800

    fixed bootstrap issue

--- frontend-web/handover/src/main.js
+++ frontend-web/handover/src/main.js
@@ -1,6 +1,7 @@
 import '/src/assets/main.css';
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { createApp } from '/node_modules/.vite/deps/vue.js?v=2584e159';
+import 'bootstrap'
+import { createApp } from 'vue';
 import App from '/src/App.vue';
 import router from '/src/router/index.js'; // Import your router configuration
 
